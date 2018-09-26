c806004052ab94101bedff3d08af63839190d050
Rajasekar M <36572895+rajasekar1999@users.noreply.github.com>
2018-09-26 21:22:59 +0530

    Create PyBot.py

---
+++ PyBot.py
@@ -0,0 +1,19 @@
+import chatterbot
+from chatterbot.trainers import ListTrainer
+import os
+
+bot = ChatBot('Bot')
+bot.set_Trainer(ListTrainer)
+
+for files in os.listdir('C:/Users/RAJASEKAR/Downloads/New Project/chatterbot-corpus-master/chatterbot_corpus/data/english'):
+	data = open('C:/Users/RAJASEKAR/Downloads/New Project/chatterbot-corpus-master/chatterbot_corpus/data/english'+ files, 'r').readlines()
+	bot.train(data)
+
+while True:
+	message = input('You: ')
+	if message.strip() != 'Bye':
+		reply = bot.get_response(message)
+		print('Bot: ', reply)
+	if message.strip() == 'Bye':
+		print('Bot: Bye')
+		break
